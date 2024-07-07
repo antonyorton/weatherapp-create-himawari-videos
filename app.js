34414323dfa9542f3b79bd08680604b230fc6b48
antonyorton <antony.orton@hotmail.com>
2024-07-07 18:41:17 +1000

    changed timing

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ app.get('/', (req, res) => {
   res.send('Hello from the video-to-s3 Express app!')
 })
 
-cron.schedule('0,10,20,30,40,43,50 * * * *', () => {
+//render (hobby plan) app is woken up at 15 past the hour from AWS lambda
+//run the cron job at 20 past the hour (others can be run at 25, 30 etc)
+cron.schedule('20 * * * *', () => {
   const myDate = new Date()
   const my_image_code = 'aus_snd'
   const my_image_prefix = image_codes[my_image_code]['image_code']
@@ -18,8 +20,6 @@ cron.schedule('0,10,20,30,40,43,50 * * * *', () => {
   createVideoFromImages((image_bucket_dir = 'weather-data/satellite/public-images'), (image_prefix = my_image_prefix))
     .then(() => push_video_to_s3((image_prefix = my_image_prefix), (local_video_loc = './tmp/temp_video.mp4'), (video_bucket_dir = 'weather-data/satellite/public-videos')))
     .then(() => console.log('Video upload complete. ', 'Node-cron task completed at time: ', myDate.toISOString()))
-  //
-  // console.log('Node-cron event complete at time: ', myDate.toISOString())
 })
 
 app.listen(port, () => {
