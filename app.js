bbd0c3fd67ccb624926b449ea2a0c585c1777240
antonyorton <antony.orton@hotmail.com>
2024-07-13 22:23:37 +1000

    remove task for hi-res full disk image due to no imagemagick on Render

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const cron = require('node-cron')
 const express = require('express')
 const { createVideoFromImages, push_video_to_s3 } = require('./create_video_from_S3_images.js')
 const { image_codes } = require('./jma_image_codes.js')
-const { create_himawari_image } = require('./create_hi_res_himawari_image.js')
 
 const app = express()
 const port = 3001
@@ -15,7 +14,7 @@ app.get('/', (req, res) => {
 // //run the cron jobs at 20, 25, 30, 35 past the hour
 
 //Cron Job 1. Fetch images for 'aus_snd':
-cron.schedule('20 * * * *', () => {
+cron.schedule('25 * * * *', () => {
   const myDate = new Date()
   const my_image_code = 'aus_snd'
   const my_image_prefix = image_codes[my_image_code]['image_code']
@@ -26,7 +25,7 @@ cron.schedule('20 * * * *', () => {
 })
 
 //Cron Job 2. Fetch images for 'se1_snd':
-cron.schedule('20 * * * *', () => {
+cron.schedule('30 * * * *', () => {
   const myDate = new Date()
   const my_image_code = 'se1_snd'
   const my_image_prefix = image_codes[my_image_code]['image_code']
@@ -37,7 +36,7 @@ cron.schedule('20 * * * *', () => {
 })
 
 //Cron Job 3. Fetch images for 'pia_snd':
-cron.schedule('20 * * * *', () => {
+cron.schedule('35 * * * *', () => {
   const myDate = new Date()
   const my_image_code = 'pia_snd'
   const my_image_prefix = image_codes[my_image_code]['image_code']
@@ -46,11 +45,6 @@ cron.schedule('20 * * * *', () => {
     .then(() => push_video_to_s3((image_prefix = my_image_prefix), (local_video_loc = './tmp/temp_video.mp4'), (video_bucket_dir = 'weather-data/satellite/public-videos')))
     .then(() => console.log(`Video upload of ${my_image_code} complete. `, 'Node-cron task completed at time: ', myDate.toISOString()))
 })
-
-// //Cron Job 4. Create hi-res image of full disk
-// cron.schedule('20 * * * *', () => {
-//   create_himawari_image().then(() => console.log('image created'))
-// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
